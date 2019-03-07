9945be71f7ff4dbcd646fa2c3516537925d8e67b
KAEL VANBREDA-FOXWELL <102675853@student.swin.edu.au>
2019-03-07 17:23:48 +1100

    Initial commit

---
+++ Hello World/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Hello_World {
+	
+	class Program {
+		
+		public static void Main(string[] args) {
+						
+			string message;
+			string name;
+			int num1;
+			int num2;
+			
+//			message = "Hello";
+//			name = Console.ReadLine();
+			num1 = 10;
+			num2 = 20;
+			
+//			Console.WriteLine(message + " " + name);
+			Console.WriteLine(num1 + num2);
+			Console.ReadKey(true);
+		}
+	}
+}
