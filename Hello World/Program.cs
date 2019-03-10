f4d266305841ffa3d0aa5682892474603383c5b1
Foxwell24 <39758803+Foxwell24@users.noreply.github.com>
2019-03-10 12:50:48 +1100

    .

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -10,16 +10,25 @@ namespace Hello_World {
 			int num2;
 			string inputStorage1;
 			string inputStorage2;
-			
+		
+			System.Console.WriteLine("Calculator for 2 numbers being added!");
+			System.Console.WriteLine("");
+			System.Console.WriteLine("Enter first number");
 			inputStorage1 = Console.ReadLine();
+			System.Console.WriteLine("Enter seccond number");
 			inputStorage2 = Console.ReadLine();
-			
+
 			num1 = int.Parse(inputStorage1);
 			num2 = int.Parse(inputStorage2);
 			
 			int output = num1 + num2;
 			
-			Console.WriteLine(output);
+			System.Console.WriteLine("Thanks! the answer is...");
+			System.Console.WriteLine(num1);
+			System.Console.WriteLine("+");
+			System.Console.WriteLine(num2);
+			System.Console.WriteLine("=");
+			System.Console.WriteLine(output);
 			Console.ReadKey(true);
 		}
 	}
