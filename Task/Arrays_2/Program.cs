277974728140c6542bc5252bfc992280215f1974
Foxwell24 <39758803+Foxwell24@users.noreply.github.com>
2019-05-02 11:04:41 +1000

    .

---
+++ Task/Arrays_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arrays_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int[] numbers = new int[10] { 34, 5, 67, 1, 99, 34, 44, 78, 34, 0 };
+
+
+            foreach (int i in numbers)
+                count += i;
+
+            Console.WriteLine(count);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task/Lists_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> primeList = new List<int> {1,2,3,4,5};
+
+            int b = 0;
+            while (b==0)
+            {
+                Console.WriteLine("Gimmi a number");
+                int number = int.Parse(Console.ReadLine());
+
+                if (primeList.Contains(number))
+                {
+                    Console.WriteLine("That number has already been used.");
+                    Console.WriteLine();
+                }
+                else{
+                    primeList.Add(number);
+                }
+            }
+
+            Console.WriteLine("Enter a number");
+            Console.ReadLine();
+        }
+    }
+}
