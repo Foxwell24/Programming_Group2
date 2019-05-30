6c8462c56b36e9071e547e2002745a5f1f18500e
Foxwell24 <39758803+Foxwell24@users.noreply.github.com>
2019-05-30 20:49:04 +1000

    .
    .

--- Term2/Bees/Program.cs
+++ Term2/Bees/Program.cs
@@ -8,25 +8,80 @@ namespace Bees
 {
     class Program
     {
+        static List<Beehive> Beehives = new List<Beehive>();
         static void Main(string[] args)
         {
-            Bee yeet = MakeBee();
+            
+            while (0 == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Make a Beehive: 1");
+                Console.WriteLine("Make a Bee    : 2");
+                string i = Console.ReadLine();
+                if (i=="1")
+                {
+                    Console.Clear();
+                    Beehive b = MakeHive();                    
+                    Beehives.Add(b);
+                }
+                else if (i == "2" && Beehives.Count >= 1)
+                {
+                    Console.Clear();
+                    Bee b = MakeBee();                    
+                }
+                else if (i == "2" && Beehives.Count < 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Bees cannot be made without a Hive to go into");
+                    Console.WriteLine("Pleese make a hive.");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, i do not know that command");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+
+                
+            }
         }
 
         public static Bee MakeBee()
         {
+            Console.Clear();
             Console.WriteLine("Bee name");
             string n = Console.ReadLine();
+
             Console.WriteLine("Bee size");
             float s = float.Parse(Console.ReadLine());
-            return new Bee(n, s);
+
+            string hives = "";
+            foreach (Beehive i in Beehives)
+            {
+                hives += "[" + Beehives[0] + "] ";
+            }
+
+            Console.WriteLine("What Hive do you want to put ["+n+"] into?");
+            Console.WriteLine("");
+            Console.WriteLine(hives);
+            string h = Console.ReadLine();
+            
+                
+            Bee b = new Bee(n, s, h);
+
+            
+            return b;
+            
         }
 
         public static Beehive MakeHive()
         {
+            Console.Clear();
             Console.WriteLine("Hive name");
-            string n = Console.ReadLine();            
-            return new Beehive(n);
+            string n = Console.ReadLine();
+            Beehive bh = new Beehive(n);
+            return bh;
         }
     }
 
@@ -39,7 +94,7 @@ namespace Bees
         {
             Name = name;
             Size = size;
-            
+            Hive = hive;
         }
     }
 
