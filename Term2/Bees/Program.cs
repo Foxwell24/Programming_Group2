ebe88ed53039d9e33a9ed1e3bf2e0e2337535356
KAEL VANBREDA-FOXWELL <102675853@student.swin.edu.au>
2019-05-30 17:36:53 +1000

    started Bees

---
+++ Term2/Bees/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bee yeet = MakeBee();
+        }
+
+        public static Bee MakeBee()
+        {
+            Console.WriteLine("Bee name");
+            string n = Console.ReadLine();
+            Console.WriteLine("Bee size");
+            float s = float.Parse(Console.ReadLine());
+            return new Bee(n, s);
+        }
+
+        public static Beehive MakeHive()
+        {
+            Console.WriteLine("Hive name");
+            string n = Console.ReadLine();            
+            return new Beehive(n);
+        }
+    }
+
+    public class Bee
+    {
+        public string Name;
+        public float Size;
+        public string Hive;
+        public Bee(string name, float size, string hive)
+        {
+            Name = name;
+            Size = size;
+            
+        }
+    }
+
+    public class Beehive
+    {
+        public string HiveName;
+        public List<Bee> Bees;
+        public Beehive(string hiveName)
+        {
+            HiveName = hiveName;
+            Bees = new List<Bee>();
+        }
+        public void AddBee(Bee b)
+        {
+            Bees.Add(b);
+        }
+    }
+}
+
